dc8241e3104c61902da60062365fa78471a70587
ccanasj <camilo.canas@twelve.net.co>
2023-04-30 18:56:53 -0500

    update predict model

--- app/ia/predict_closing_stock.py
+++ app/ia/predict_closing_stock.py
@@ -1,13 +1,9 @@
-from sklearn.preprocessing import MinMaxScaler
-import numpy as np
 import yfinance as yf
-from datetime import date, timedelta, datetime
-import keras
+from datetime import timedelta
+from prophet import Prophet
 
 
 def predict(symbol: str, start_date: str, predict_days: int):
-    # Load the saved model
-    saved_model = keras.models.load_model('app/model/stock_prediction_model.h5')
 
     # Get the stock data from yfinance
     new_df = yf.download(symbol, start=start_date, progress=False)
@@ -15,19 +11,17 @@ def predict(symbol: str, start_date: str, predict_days: int):
     new_dataset = new_data.values
     dates = new_df.index.to_list()
     
-    # Scale the new data using the same scaler used during training
-    scaler = MinMaxScaler(feature_range=(0,1))
-    new_scaled_data = scaler.fit_transform(new_dataset)
+    df = new_df.reset_index()[["Date", "Close"]]
+    df = df.rename({"Date": "ds", "Close": "y"}, axis=1)
+    model = Prophet(daily_seasonality=True)
+    model.fit(df)
+    
+    future_dates = model.make_future_dataframe(periods=predict_days, freq='D')
+    # Generate predictions for the future dates
+    forecast = model.predict(future_dates)
 
-    # Predict the next 30 days of closing values
-    next_predictions = []
-    for _ in range(predict_days):
-        next_prediction = saved_model.predict(np.array([new_scaled_data[-60:, 0]]).reshape(1, 60, 1))
-        next_predictions.append(next_prediction)
-        new_scaled_data = np.append(new_scaled_data, next_prediction, axis=0)
-
-    # Scale the next predictions back to original values
-    next_predictions = scaler.inverse_transform(np.array(next_predictions).reshape(-1, 1))
+    # Extract the predicted closing values for the next 60 days
+    predicted_closing_values = forecast.tail(predict_days)["yhat"].values
 
     next_date = dates[-1]
     
@@ -38,4 +32,4 @@ def predict(symbol: str, start_date: str, predict_days: int):
             next_date += timedelta(days=1)
         dates.append(next_date)
     
-    return new_dataset, next_predictions, dates
+    return new_dataset, predicted_closing_values, dates
--- app/routers/predict_closing_stock.py
+++ app/routers/predict_closing_stock.py
@@ -11,9 +11,9 @@ predict_router = APIRouter(prefix="/api/v1/predict",
 async def suggestions(symbol: str, start_date: str = "2020-01-01", predict_days: int = 30):
 
     closing_price, predicted_closing_price, dates = predict(symbol, start_date, predict_days)
-    
+
     return {
                 "dates": dates,
-                "predicted_closing_price": predicted_closing_price.ravel().tolist(),
+                "predicted_closing_price": predicted_closing_price.tolist(),
                 "closing_price": closing_price.ravel().tolist(),
             }
